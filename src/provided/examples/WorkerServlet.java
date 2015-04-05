d71c3ca99f7dd244abf0c85fbfcb43b737cff0ee
Alan Wootton <alan@gotohere.com>
2015-04-05 00:41:22 -0700

    WWC is up at ec2

--- src/provided/examples/WorkerServlet.java
+++ src/provided/examples/WorkerServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.gwems.Push2Client;
 import org.gwems.servers.ClusterState;
 import org.gwems.servers.Global;
 import org.messageweb.dynamo.LastTimeItem;
@@ -28,16 +29,14 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 /**
- * An example Amazon Elastic Beanstalk Worker Tier application. This example
- * requires a Java 7 (or higher) compiler.
+ * An example Amazon Elastic Beanstalk Worker Tier application. This example requires a Java 7 (or higher) compiler.
  * 
  * atw - it needs to stay in the default package unless the web.xml is changed.
  * 
  * dynamo = new AmazonDynamoDBClient(credentialsProvider);
  * 
- * 
  */
-@WebServlet(urlPatterns = {"/Serve"}, loadOnStartup = 1)
+@WebServlet(urlPatterns = { "/Serve" }, loadOnStartup = 1)
 public class WorkerServlet extends HttpServlet {
 
 	private static Logger logger = Logger.getLogger(WorkerServlet.class);
@@ -48,44 +47,73 @@ public class WorkerServlet extends HttpServlet {
 	public WorkerServlet() {
 
 		logger.debug(" . . . . . . . . . . . . . . . . . . . . . . . . . . . . . Starting");
-		
-		// what is the name of the cluster??? 
-		// what is the location of the redis here?
-		// whar are the addresses of the other clusters? (get from db - oops, can't !)
 
-		Global global = new Global(8081, new ClusterState());// starts a ws
-																// server
-		logger.debug(" -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -* Started "
-				+ global);
+		// what is the name of the cluster???
+		// what is the location of the redis here?
+
+		ClusterState cluster = new ClusterState();
+		cluster.redis_server = "localhost";// the default
+		cluster.redis_port = 6379;
+
+		Global global = new Global(8081, cluster);
+
+		logger.debug("-* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -*  -* -* Started " + global);
+
+		TimePusher pusher = new TimePusher();
+		pusher.global = global;
+		Thread t = new Thread(pusher);
+		t.start();
+	}
+
+	private class TimePusher implements Runnable {
+		Global global;
+
+		@Override
+		public void run() {
+			// publish the time every 10 sec.
+			long time_10 = System.currentTimeMillis() + 10 * 1000;
+			long time_60 = System.currentTimeMillis() + 22 * 1000;
+			while (true) {
+				long time = System.currentTimeMillis();
+				if (time > time_10) {
+					time_10 += 10 * 1000;
+					global.publish("WWC" + "#TimeEveryTenSeconds", new Push2Client("" + new Date()));
+					global.publish("WWC" + "#10secs", new Push2Client("" + new Date()));
+					// logger.info("sent time to #TimeEveryTenSeconds");
+				}
+				if (time > time_60) {
+					time_60 += 60 * 1000;
+					global.publish("WWC" + "#TimeEveryMinute", new Push2Client("" + new Date()));
+					// logger.info("sent time to #TimeEveryMinute");
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
 	}
 
 	/**
-	 * A client to use to access Amazon S3. Pulls credentials from the
-	 * {@code AwsCredentials.properties} file if found on the classpath,
-	 * otherwise will attempt to obtain credentials based on the IAM Instance
-	 * Profile associated with the EC2 instance on which it is run.
+	 * A client to use to access Amazon S3. Pulls credentials from the {@code AwsCredentials.properties} file if found
+	 * on the classpath, otherwise will attempt to obtain credentials based on the IAM Instance Profile associated with
+	 * the EC2 instance on which it is run.
 	 */
-	private final AmazonS3Client s3 = new AmazonS3Client(
-			new AWSCredentialsProviderChain(
-					new InstanceProfileCredentialsProvider(),
-					new ClasspathPropertiesFileCredentialsProvider()));
+	private final AmazonS3Client s3 = new AmazonS3Client(new AWSCredentialsProviderChain(new InstanceProfileCredentialsProvider(),
+			new ClasspathPropertiesFileCredentialsProvider()));
 
 	/**
-	 * This method is invoked to handle POST requests from the local SQS daemon
-	 * when a work item is pulled off of the queue. The body of the request
-	 * contains the message pulled off the queue.
+	 * This method is invoked to handle POST requests from the local SQS daemon when a work item is pulled off of the
+	 * queue. The body of the request contains the message pulled off the queue.
 	 */
 	@Override
-	protected void doPost(final HttpServletRequest request,
-			final HttpServletResponse response) throws ServletException,
-			IOException {
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 
 		try {
 
 			// Parse the work to be done from the POST request body.
 
-			WorkRequest workRequest = WorkRequest.fromJson(request
-					.getInputStream());
+			WorkRequest workRequest = WorkRequest.fromJson(request.getInputStream());
 
 			// Simulate doing some work.
 
@@ -95,8 +123,7 @@ public class WorkerServlet extends HttpServlet {
 
 			byte[] message = workRequest.getMessage().getBytes(UTF_8);
 
-			s3.putObject(workRequest.getBucket(), workRequest.getKey(),
-					new ByteArrayInputStream(message), new ObjectMetadata());
+			s3.putObject(workRequest.getBucket(), workRequest.getKey(), new ByteArrayInputStream(message), new ObjectMetadata());
 
 			// Signal to beanstalk that processing was successful so this work
 			// item should not be retried.
@@ -111,27 +138,23 @@ public class WorkerServlet extends HttpServlet {
 			// when writing to Amazon S3).
 
 			response.setStatus(500);
-			try (PrintWriter writer = new PrintWriter(
-					response.getOutputStream())) {
+			try (PrintWriter writer = new PrintWriter(response.getOutputStream())) {
 				exception.printStackTrace(writer);
 			}
 		}
 	}
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getParameter("path");//
 		System.out.println("Str is " + path);
 
-		AmazonDynamoDBClient dynamo = new AmazonDynamoDBClient(
-				new AWSCredentialsProviderChain(
-						new InstanceProfileCredentialsProvider(),
-						new ClasspathPropertiesFileCredentialsProvider()));
+		AmazonDynamoDBClient dynamo = new AmazonDynamoDBClient(new AWSCredentialsProviderChain(new InstanceProfileCredentialsProvider(),
+				new ClasspathPropertiesFileCredentialsProvider()));
 
 		dynamo.setRegion(Region.getRegion(Regions.US_WEST_2));
-		
+
 		DynamoDBMapper mapper = new DynamoDBMapper(dynamo);
-		
+
 		String last = "none";
 		String now = "" + new Date();
 
@@ -144,40 +167,38 @@ public class WorkerServlet extends HttpServlet {
 		b.setWhen("" + new Date());
 		mapper.save(b);
 
-		// an ecxample of doing it the hard way. 
-//		Map<String, AttributeValue> keyMap = new HashMap<String, AttributeValue>();
-//		keyMap.put("id", new AttributeValue("dummyKey123"));
-//
-//		GetItemResult result = null;
-//		try {
-//			result = dynamo.getItem("AtwTable2", keyMap);
-//		} catch (AmazonServiceException e) {
-//			e.printStackTrace();
-//		} catch (AmazonClientException e) {
-//			e.printStackTrace();
-//		}
-//		String last = "none";
-//		String now = "" + new Date();
-//		if (result != null) {
-//			AttributeValue aval = result.getItem().get("when");
-//			if (aval != null) {
-//				last = "" + aval;
-//			}
-//		}
-//		keyMap.put("when", new AttributeValue(now));
-//		PutItemResult res;
-//		try {
-//			res = dynamo.putItem("AtwTable2", keyMap);
-//			System.out.println(" got result " + res.toString());
-//		} catch (AmazonServiceException e) {
-//			e.printStackTrace();
-//		} catch (AmazonClientException e) {
-//			e.printStackTrace();
-//		}
+		// an ecxample of doing it the hard way.
+		// Map<String, AttributeValue> keyMap = new HashMap<String, AttributeValue>();
+		// keyMap.put("id", new AttributeValue("dummyKey123"));
+		//
+		// GetItemResult result = null;
+		// try {
+		// result = dynamo.getItem("AtwTable2", keyMap);
+		// } catch (AmazonServiceException e) {
+		// e.printStackTrace();
+		// } catch (AmazonClientException e) {
+		// e.printStackTrace();
+		// }
+		// String last = "none";
+		// String now = "" + new Date();
+		// if (result != null) {
+		// AttributeValue aval = result.getItem().get("when");
+		// if (aval != null) {
+		// last = "" + aval;
+		// }
+		// }
+		// keyMap.put("when", new AttributeValue(now));
+		// PutItemResult res;
+		// try {
+		// res = dynamo.putItem("AtwTable2", keyMap);
+		// System.out.println(" got result " + res.toString());
+		// } catch (AmazonServiceException e) {
+		// e.printStackTrace();
+		// } catch (AmazonClientException e) {
+		// e.printStackTrace();
+		// }
 
-		response.getWriter().println(
-				"WorkerServlet static assets path =" + path + " last val was "
-				 + last + " current val is " + now);
+		response.getWriter().println("WorkerServlet static assets path =" + path + " last val was " + last + " current val is " + now);
 	}
 
 }
